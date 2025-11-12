821618c00d2109a93f9de19359264474c5940fd5
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-12 08:52:57 +0100

    added validator to the create user class

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsEnum, IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+    
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+    
+    @IsEnum(['INTERN', 'ENGINEER', 'ADMIN'],{
+        message: 'Role must Valid',
+    })
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
 }
