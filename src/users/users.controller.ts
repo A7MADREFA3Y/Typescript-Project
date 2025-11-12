ecee6b250f62774efa0f978bc9b7e7c7cdbd014e
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-12 05:52:12 +0100

    connected the user service to the uesr contorller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,30 +4,32 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService){}
+
     @Get() // Get all users
     findAll(@Query('role')role?:'INTERN' | 'ENGINEER' | 'ADMIN' ) {
-        return [];
+        return this.usersService.findAll(role);
     }
 
     @Get (':id') // Get a single user by ID
     findOne(@Param('id') id: string) {
-        return {id};
+        return this.usersService.findOne(+id);
     }    
 
     @Post() // Create a new user
-    create(@Body() user: {}) {
-        return user;
+    create(@Body() user: {name : string, email : string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.create(user);
     }
 
 
     @Patch (':id') // Patch /users/:id
-    update(@Param('id') id: string, @Body() userupdate: {}) {
-        return {id, ...userupdate};
+    update(@Param('id') id: string, @Body() userupdate: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.update(+id, userupdate);
     } 
 
     @Delete (':id') // Delete /users/:id
     remove(@Param('id') id: string) {
-        return {deleted: id};
+        return this.usersService.delete(+id);
     }
 
 
